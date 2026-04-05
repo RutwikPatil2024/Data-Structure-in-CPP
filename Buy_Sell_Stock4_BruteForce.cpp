5bd00928d2d92e2aa18b83c3d8c19639bb250a66
RutwikPatil2024 <rutwikpatil8000@gmail.com>
2026-04-05 20:57:52 +0530

    Buy and Sell Stock Brute Force

---
+++ Buy_Sell_Stock4_BruteForce.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int f(vector<int>prices,int i,bool on,int k)
+{
+    if(i==prices.size() or k==0){
+        return 0;
+    }
+    
+    if(on){ //you have stock so you can sell or skip
+        return max(f(prices,i+1,on,k),prices[i]+f(prices,i+1,false,k-1));
+    }
+    else{//you dont have stock so you can buy or skip
+        return max(f(prices,i+1,on,k),-prices[i]+f(prices,i+1,true,k));
+    }
+}
+int main()
+{
+    vector<int> prices = {2,4,1};
+    cout << f(prices, 0, false, 2);
+    return 0;
+}
